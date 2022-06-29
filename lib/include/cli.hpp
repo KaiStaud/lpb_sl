2358b5c97033baf43381e75a1543c7b58850c381
kai staud <kai-staud@web.de>
2022-06-29 21:44:30 +0200

    CLI is running in seperate task

--- lib/include/cli.hpp
+++ lib/include/cli.hpp
@@ -18,7 +18,13 @@
 #include <vector>
 
 using namespace tracking;
-
+enum class ParsingErrors
+{
+     no_error = 0,
+     value_out_of_bound = 1,
+     incorrect_input = 2,
+     incorrect_format = 3,
+};
 /**
  * @brief Abstraction class for cli functionality
  *
@@ -35,22 +41,16 @@ public:
     void hot_reload_config();                /*< check if file changed */
     /* Options */
     int execute(int argc, char **argv); /**< Run the cli parser (again) */
-    /* Transfer functions from cli to db.
-        The database may be busy and/or locked by other threads.
-    */
-    void try_create_star();          /**< Create a star*/
-    void try_create_constellation(); /**< Create a constellaton*/
-    void try_create_track();         /**< Create a star*/
+
 
 private:
     // Bind methods with std::function
     void create_defconfig(); /**< Create default config, if not exists */
-
-//     std::variant<star, ParsingErrors> QueryStar(); /**< Read in star from command line */
-//     std::variant<constellation, ParsingErrors> QueryConstellation(std::uint8_t num_stars = 3); /**< Read in complete constellation */
-//     std::variant<track, ParsingErrors> QueryTrack(std::uint8_t num_constellations = 1); /** < Create complete Track from cli */
-   tstar query_star(); /**< Read in star from command line */
-   constellation query_constellation(std::uint8_t num_stars = 3); /**< Read in complete constellation */
-   track query_track(std::uint8_t num_constellations = 1); /** < Create complete Track from cli */
+    /* Transfer functions from cli to db.
+        The database may be busy and/or locked by other threads.
+    */
+     std::variant<tstar, ParsingErrors> QueryStar(); /**< Read in star from command line */
+     std::variant<constellation, ParsingErrors> QueryConstellation(std::uint8_t num_stars = 3); /**< Read in complete constellation */
+     std::variant<track, ParsingErrors> QueryTrack(std::uint8_t num_constellations = 1); /** < Create complete Track from cli */
 };
 
